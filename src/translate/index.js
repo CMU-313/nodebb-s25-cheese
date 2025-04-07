9a6c6edec4ab1cfdb4aca130dde4d489823dc15e
jurias <jurias@andrew.cmu.edu>
2025-04-06 21:29:12 -0400

    Added setup for UI and translation integration

--- src/translate/index.js
+++ src/translate/index.js
@@ -3,6 +3,7 @@ var request = require('request');
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
+    console.log('[Translate] Called with content:', postData.content);
     const TRANSLATOR_API = "http://translator:5000/"
     const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
     const data = await response.json();
